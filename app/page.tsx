6fcbfb03e110d94d2c16ef906c622f7259d848f7
BruskiMac <caleb.a.bruski@gmail.com>
2024-07-16 15:16:36 +0300

    added some glam

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,64 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
+const napoleonQuotes = [
+  "Tina, come get some ham.",
+  "Whatever I feel like I wanna do. Gosh!",
+  "It’s pretty much my favorite animal. It’s like a lion and a tiger mixed…",
+  "A freakin' 12-gauge, what do you think?",
+  "The worst day of my life, what do you think?",
+  "Stay home and eat all the freakin' chips, Kip.",
+  "What the flip was Grandma doing at the sand dunes?",
+  "Ugh. Kip hasn't done flipping anything today!",
+  "Girls only want boyfriends who have great skills!",
+  "Lucky!",
+  "Yes, like Napoleon, dynamite!",
+  "Napoleon, don't be jealous that I've been chatting online with babes all day.",
+  "You know, like nunchuck skills, bow hunting skills, computer hacking skills...",
+  "Vote for Pedro!",
+  "I caught you a delicious bass.",
+  "Nunchuck skills... bowhunting skills... computer hacking skills... Girls only want boyfriends who have great skills.",
+  "I see you're drinking 1%. Is that 'cause you think you're fat? 'Cause you're not. You could be drinking whole if you wanted to.",
+  "Your mom goes to college.",
+  "Pedro offers you his protection.",
+  "Give me some of your tots!",
+  "Gosh! Idiot!",
+  "You ever take it off any sweet jumps?",
+  "Just borrow some from the school nurse. I know she has like five sticks in her drawer.",
+  "Can you bring me my chapstick?",
+  "Napoleon, don't be jealous that I've been chatting online with babes all day. Besides, we both know that I'm training to be a cage fighter.",
+  "Tina, you fat lard, come get some dinner!",
+  "Eat the food, Tina!",
+  "The worst day of my life, what do you think?",
+  "I don't even have any good skills. You know, like nunchuck skills, bowhunting skills, computer hacking skills. Girls only want boyfriends who have great skills!",
+  "You got like three feet of air that time.",
+  "Freakin' idiot!",
+  "So, we're pretty much friends by now, right?",
+  "You're such an idiot.",
+  "Stay home and eat all the freakin' chips, Kip.",
+  "What the flip was Grandma doing at the sand dunes?",
+  "I could build you a cake or somethin'.",
+  "Why don't you go tell your mom to shut up?",
+  "It's pretty much my favorite animal. It's like a lion and a tiger mixed... bred for its skills in magic.",
+  "I don't feel very good.",
+  "You're not my mom, Don.",
+  "No, it's a sledgehammer.",
+  "My lips hurt real bad!",
+  "Who's the only one here who knows the illegal ninja moves from the government? (raises hand slowly) Me.",
+];
+
 export default function Home() {
   const generateRandomNumbers = () => {
     return Array.from({ length: 8 }, () => Math.floor(Math.random() * 6) + 1);
   };
 
   const [numbers, setNumbers] = useState<number[]>(generateRandomNumbers());
-
   const [savedDigits, setSavedDigits] = useState<Set<number>>(new Set());
-
   const [numberOfRolls, setNumberOfRolls] = useState<number>(0);
-
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [quotation, setQuotation] = useState<string>(
+    " Whatever I feel like i wanna do, Gosh"
+  );
 
   const rollDice = () => {
     setNumberOfRolls((prevNumberOfRolls) => prevNumberOfRolls + 1);
@@ -24,6 +70,13 @@ export default function Home() {
         return Math.floor(Math.random() * 6) + 1;
       })
     );
+    randomQuote();
+  };
+
+  const randomQuote = () => {
+    const randomQuoteIndex = Math.floor(Math.random() * napoleonQuotes.length);
+    const randomQuoteElement = napoleonQuotes[randomQuoteIndex];
+    setQuotation(randomQuoteElement);
   };
 
   const toggleSaveDigit = (index: number) => {
@@ -89,6 +142,10 @@ export default function Home() {
         Number of Rolls: {numberOfRolls}
       </h2>
 
+      <div className="mt-4 text-center">
+        <h3 className="text-gray-700 text-lg mb-2">{quotation}</h3>
+      </div>
+
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-800 bg-opacity-50">
           <div className="bg-white p-8 max-w-md mx-auto rounded-lg shadow-lg text-center">
@@ -96,12 +153,20 @@ export default function Home() {
             <p className="text-gray-700 mb-4">
               Number of Rolls: {numberOfRolls}
             </p>
+            <iframe
+              title="Giphy GIF"
+              allow="fullscreen"
+              frameBorder="0"
+              height="270"
+              src="https://giphy.com/embed/VawNgfFSDS4PMl1vHK/video"
+              className="max-w-full h-auto mx-auto"
+            ></iframe>
             <button
               onClick={() => {
                 setShowModal(false);
                 resetScore();
               }}
-              className="px-4 py-2 bg-blue-500 text-white rounded"
+              className="px-4 py-2 bg-blue-500 text-white rounded mt-4"
             >
               Play Again
             </button>
