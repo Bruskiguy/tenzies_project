55e90cc57a257f0f6698d795de5bb579befdb3a3
BruskiMac <caleb.a.bruski@gmail.com>
2024-07-16 15:34:27 +0300

    added X button to module

--- app/page.tsx
+++ app/page.tsx
@@ -108,6 +108,11 @@ export default function Home() {
     }
   }, [numbers, savedDigits]);
 
+  const closeModal = () => {
+    setShowModal(false);
+    resetScore();
+  };
+
   return (
     <main className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
       <h1 className="text-gray-700 text-xl font-bold mb-4">Tenzies!</h1>
@@ -153,11 +158,26 @@ export default function Home() {
 
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-800 bg-opacity-50">
-          <div className="bg-white p-8 max-w-md mx-auto rounded-lg shadow-lg text-center">
-            <h2 className="text-gray-700 text-xl font-bold mb-4">You Win!</h2>
-            <p className="text-gray-700 mb-4">
-              Number of Rolls: {numberOfRolls}
-            </p>
+          <div className="bg-white p-8 max-w-md mx-auto rounded-lg shadow-lg text-center relative">
+            <button
+              onClick={closeModal}
+              className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
             <iframe
               title="Giphy GIF"
               allow="fullscreen"
@@ -166,12 +186,13 @@ export default function Home() {
               src="https://giphy.com/embed/VawNgfFSDS4PMl1vHK/video"
               className="max-w-full h-auto mx-auto"
             ></iframe>
+            <h2 className="text-gray-700 text-xl font-bold mb-4">You Win!</h2>
+            <p className="text-gray-700 mb-4">
+              Number of Rolls: {numberOfRolls}
+            </p>
             <button
-              onClick={() => {
-                setShowModal(false);
-                resetScore();
-              }}
-              className="px-4 py-2 bg-blue-500 text-white rounded mt-4"
+              onClick={closeModal}
+              className="px-4 py-2 bg-blue-500 text-white rounded"
             >
               Play Again
             </button>
